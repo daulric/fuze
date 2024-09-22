4836630c536544d0a43bebc57aee00203cbd7dea
Ulric <ulricaird1@gmail.com>
2024-09-22 19:29:01 -0400

    initialize supabase and set up authentication route

---
+++ src/app/api/auth/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return new NextResponse("ok");
+}
+
+export async function POST() {
+    // Sign up
+}
+
+export async function PUT() {
+    // Login
+}
+
+export async function DELETE() {
+    // Logout
+}
