a313759c03f6f904df377a57dabb3933eac11a22
Ulric <ulricaird1@gmail.com>
2024-10-08 22:44:28 -0400

    added to change

--- src/app/video/page.jsx
+++ src/app/video/page.jsx
@@ -1,15 +1,29 @@
-import { Suspense } from "react"
-import MainVideoPage from "./Main"
+import { Suspense } from 'react';
+import MainVideoPage from './Main';
+import { headers } from 'next/headers';
 
-export const metadata = {
-    title: "zTube - Video",
-    description: "Watch a video"
+export async function generateMetadata({ searchParams }) {
+  const headersList = headers();
+  const host = headersList.get('host');
+  const protocol = headersList.get('x-forwarded-proto') || 'http';
+  const domain = `${protocol}://${host}`;
+
+  const video_id = searchParams.id;
+
+  const response = await fetch(`${domain}/api/video`);
+  const video_data = await response.json();
+  const item = video_data.data.filter((i) => i.video_id === video_id)[0];
+
+  return {
+    title: `${item ? item.title : 'Video'} - zTube`,
+    description: `${item ? item.description : 'Video description'}`
+  };
 }
 
 export default function PAGE() {
-    return (
-        <Suspense>
-            <MainVideoPage />
-        </Suspense>
-    )
+  return (
+    <Suspense>
+      <MainVideoPage />
+    </Suspense>
+  );
 }
