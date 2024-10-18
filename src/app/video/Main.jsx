ca4c102b6c240e2955df345cc5f3a43825ebf4ee
Ulric <ulricaird1@gmail.com>
2024-10-18 14:54:25 -0400

    Fix the Video Space Error

--- src/app/video/Main.jsx
+++ src/app/video/Main.jsx
@@ -1,9 +1,9 @@
 import VideoDisplay from "./VideoDisplay"
 
+// Main Video Component For Displaying Videos
 export default function VideoIDDisplay() {
     return (
         <div className="h-screen bg-gray-900 flex flex-col items-center justify-center p-4">
-            <br/><br/>
             <VideoDisplay />
         </div>
     );
