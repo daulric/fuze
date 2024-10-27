cb34d4b92d26c35d2a5c6806f828aeb17c658e36
Ulric <ulricaird1@gmail.com>
2024-10-27 05:26:07 -0400

    Working on Account API

--- src/app/api/settings/account/route.js
+++ src/app/api/settings/account/route.js
@@ -1,5 +1,29 @@
 import { NextResponse } from "next/server"
+//import SupabaseServer from "@/supabase/server";
 
 export async function GET() {
     return new NextResponse("ok");
+}
+
+// Make updates to the user account details.
+export async function POST(request) {
+
+    try {
+        const { username, password, account_id} = await request.body();
+
+        if (!account_id) {
+            throw "Must have Account ID";
+        }
+
+        if (!username || !password) {
+            throw "Can't Get Required Fields";
+        }
+
+    } catch (err) {
+        return NextResponse.json({
+            success: false,
+            message: err
+        })
+    }
+
 }
