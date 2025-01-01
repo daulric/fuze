2c6e51d3480053ceb4e18e748412cba167bf010a
Ulric Aird <ulricaird1@gmail.com>
2025-01-01 06:57:34 -0400

    Improved the Recommended Api

--- src/app/api/video/recommend/route.js
+++ src/app/api/video/recommend/route.js
@@ -19,7 +19,14 @@ export async function POST(request) {
 
             return data.filter(i => i.is_private !== true);
         }).then((data) => {
-            const shuffled = data.sort(() => Math.random() - 0.5);
+            const shuffled = [...data];
+
+            for (let i =  shuffled.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i+1));
+                [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+            }
+
+
             return { data: shuffled.slice(0, limit)};
         });
         
