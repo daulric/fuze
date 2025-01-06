ac5458725c88e8498ff01d2c809a3949e563b0ec
Ulric <57965846+daulric@users.noreply.github.com>
2025-01-06 15:24:00 -0400

    Cache Revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,7 @@ export default async function Home() {
   const url = await getURL();
 
   const response = await fetch(`${url}/api/video/recommend`, {
+    { next: { revalidate: 0 } }
     body: JSON.stringify({limit: 16}),
     method: "POST",
   });
