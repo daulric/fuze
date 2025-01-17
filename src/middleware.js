889b11cb09f50a1b4341b09dfce11b27c1a1735b
Ulric Aird <ulricaird1@gmail.com>
2025-01-17 15:57:27 -0400

    Cache Control

--- src/middleware.js
+++ src/middleware.js
@@ -2,10 +2,15 @@ import { NextResponse } from "next/server"
 
 // Middleware for future use
 export function middleware(request) {
+  const response = NextResponse.next();
   const { pathname, searchParams } = new URL(request.url);
+
+  response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  response.headers.set("Pragma", "no-cache");
+  response.headers.set("Expires", "0");
+  response.headers.set("Surrogate-Control", "no-store")
   
   if (pathname === "/video") {
-    
     // Video ID to Dynamic
     if (searchParams.has("id")) {
       let id = searchParams.get("id");
@@ -13,5 +18,5 @@ export function middleware(request) {
     }
   }
   
-  return NextResponse.next();
+  return response;
 }
