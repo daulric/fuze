20981953d1ba27cf8bea365738f952454d2c3dce
Ulric Aird <ulricaird1@gmail.com>
2025-03-03 08:01:56 -0400

    Attempt at Fixing Vercel Preview Server

--- src/lib/geturl.ts
+++ src/lib/geturl.ts
@@ -2,10 +2,19 @@ import { headers } from "next/headers";
 
 async function getURL() {
     const headersList = await headers();
-    const host = headersList.get('host');
-    const protocol = headersList.get('x-forwarded-proto') || 'http';
-    const url = `${protocol}://${host}`;
+    let host = headersList.get('host') || process.env.VERCEL_URL;
+    let protocol = headersList.get('x-forwarded-proto') || 'https';
 
+    if (!host) {
+        host = "localhost:3000"
+    }
+
+    if (host?.startsWith("192.168.") || host?.startsWith("10.") || host?.startsWith("172.")) {
+        protocol = "http"
+    }
+
+    const url = `${protocol}://${host}`;
+    console.log(url);
     return url;
 };
 
