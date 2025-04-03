74a010623a43c97b84660610801608f26a76209c
Ulric Aird <ulricaird1@gmail.com>
2025-04-02 20:58:00 -0400

    changes

--- src/supabase/server.js
+++ src/supabase/server.js
@@ -1,6 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr"
 
-const supabase_url = process.env.NEXT_PUBLIC_supabase_url || "example";
+const supabase_url = process.env.NEXT_PUBLIC_supabase_url || "https://example.com";
 const anon_key = process.env.NEXT_PUBLIC_supabase_anon_key || "example";
 
 const supabase = createBrowserClient(supabase_url, anon_key, {realtime: true});
